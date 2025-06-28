4f7db3df65adc7b600e886e988b6dfa0d3e18156
Roman Korchnev <romo4ko.mail@gmail.com>
2025-06-28 13:10:31 +0300

    unused routes removed

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\ChallengeController;
-use App\Http\Controllers\Api\TeamController;
 use App\Http\Controllers\Api\TelegramController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
@@ -16,10 +14,6 @@ Route::group(['middleware' => ['auth:sanctum']], static function () {
     Route::group(['prefix' => 'users'], static function () {
         Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
         Route::post('/{id}', [UserController::class, 'update'])->name('users.update');
-        Route::get('/{id}/teams', [UserController::class, 'team'])->name('users.team');
-        Route::get('/{id}/teams/is-captain', [UserController::class, 'teamIsCaptain'])->name('users.teamIsCaptain');
-        Route::get('/{id}/challenges', [UserController::class, 'challenge'])->name('users.challenge');
-        Route::get('/{id}/achievements', [UserController::class, 'achievement'])->name('users.achievement');
     });
 });
 
